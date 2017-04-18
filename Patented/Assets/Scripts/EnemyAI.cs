9a58fb99e479f08615896bff2b918589746e59aa
ajhlsu <aljherbert@gmail.com>
2017-04-17 23:35:55 -0500

    Added Enemy AI

--- Patented/Assets/Scripts/EnemyAI.cs
+++ Patented/Assets/Scripts/EnemyAI.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyKill : MonoBehaviour {
+public class EnemyAI : MonoBehaviour {
 
 
     Animator anim;
+    Rigidbody2D rigidBody;
+    Vector2 currentPosition;
+    Vector2 initialPosition;
+
+    private void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        initialPosition = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        currentPosition = initialPosition;
+        currentPosition.x = initialPosition.x + Mathf.Sin(Time.timeSinceLevelLoad);
+        rigidBody.MovePosition(currentPosition);
+    }
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Animator anim;
